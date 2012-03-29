8880f91fdb629e131b7b2df961dc230e2871d7c5
Ryan Tyler <ryan@concrete5.org>
2012-03-29 13:37:32 -0700

    no redirects for post

--- packages/mobile_theme/models/mobile_theme.php
+++ packages/mobile_theme/models/mobile_theme.php
@@ -13,36 +13,32 @@ class MobileTheme {
 		}
 		
 		if(self::isMobileDevice()) {
-			define('MOBILE_THEME_IS_ACTIVE',true); // define a constant so developers can base some logic off of it.
-
 			// we add mobile=1 if we detect mobile
 			// we then check for it so we don't get
 			// a loop
-			if(empty($_GET['mobile'])) {
+			if($_SERVER['REQUEST_METHOD'] !="POST" && empty($_GET['mobile'])) {
 				$urlh = Loader::helper('url');
 				$url = $urlh->setVariable('mobile', '1');
-
-				header('Location: ' . $url);
+				header('Location: ' . $url); exit;
 			}
+			self::setMobileTheme();
 		}
 	}
 
 	// this is also attached to the
 	// on_page_view event
-	public static function changeOnMobileURI() {
-		if(!empty($_GET['mobile'])) {
-			define('MOBILE_THEME_IS_ACTIVE',true);
-			$pkg = Package::getByHandle('mobile_theme');
-			$themeId = $pkg->config('MOBILE_THEME_ID');
-			$mobileTheme = PageTheme::getByID($themeId);
-			if($mobileTheme instanceof PageTheme) {
-				// we have to grab the instance of the view
-				// since on_page_view doesn't give it to us
-				$view = View::getInstance();
-				$view->setTheme($mobileTheme);
-			}
-			$view->addHeaderItem(Loader::helper('html')->css('ccm.app.mobile.css','mobile_theme'));
+	protected static function setMobileTheme() {
+		define('MOBILE_THEME_IS_ACTIVE',true);
+		$pkg = Package::getByHandle('mobile_theme');
+		$themeId = $pkg->config('MOBILE_THEME_ID');
+		$mobileTheme = PageTheme::getByID($themeId);
+		if($mobileTheme instanceof PageTheme) {
+			// we have to grab the instance of the view
+			// since on_page_view doesn't give it to us
+			$view = View::getInstance();
+			$view->setTheme($mobileTheme);
 		}
+		$view->addHeaderItem(Loader::helper('html')->css('ccm.app.mobile.css','mobile_theme'));		
 	}
 
 	protected static function isMobileDevice() {
